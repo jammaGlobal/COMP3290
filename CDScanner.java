b5eb960dd6f2677020bfe7aebbcb1f58308965f7
jammaGlobal <jam2evos@hotmail.com>
2020-08-18 20:27:22 +1000

    scanner change

--- CDScanner.java
+++ CDScanner.java
@@ -5,17 +5,30 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public enum STATE{
     SCAN, IDENT, DELIM_OPERATOR, S_COMMENT, M_COMMENT, KEYWORD, INTEGER, FLOAT, STRINGCONST
 }
 
-public class CD20Scanner(String input){
-    System.out.println(input);
+public class CDScanner{
+    private char[] buffer;
+
+    public CDScanner(String input){
+        System.out.println(input);
+    }
+
+
+    
 }
 
 
 public Token scan(){
+
     
+}
+
+public boolean isBufferEmpty(){
+
 }
 
 
